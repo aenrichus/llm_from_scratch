bdd05b5c7b854064299206b2135a16e0d272628d
Henry Wolf <henry@henrywolf.ai>
2025-04-26 14:46:01 -1000

    added simple tokenizer v2

--- tokenizing.py
+++ tokenizing.py
@@ -19,7 +19,9 @@ tokens = [t.strip() for t in pieces if t.strip()]
 vocab = { token: idx 
           for idx, token in enumerate(sorted(set(tokens))) }
 
-# 3) Simple tokenizer class
+print("Vocab size    :", len(vocab))
+
+# 3) Simple tokenizer class v1
 class SimpleTokenizerV1:
     def __init__(self, vocab):
         self.str_to_int = vocab
@@ -27,17 +29,45 @@ class SimpleTokenizerV1:
 
     def encode(self, text):
         pieces = re.split(r'([,.?_!"()\']|--|\s)', text)
-        toks   = [t.strip() for t in pieces if t.strip()]
-        return [ self.str_to_int[t] for t in toks ]
+        tokens = [t.strip() for t in pieces if t.strip()]
+        return [ self.str_to_int[t] for t in tokens ]
 
     def decode(self, ids):
         txt = " ".join(self.int_to_str[i] for i in ids)
         # glue punctuation back on
         return re.sub(r'\s+([,.?!"()\'])', r'\1', txt)
 
-# 4) Test
+# Test
 tokenizer = SimpleTokenizerV1(vocab)
 ids = tokenizer.encode(raw_text)
 print("First 20 tokens:", tokens[:20])
 print("First 20 IDs   :", ids[:20])
-print("Vocab size    :", len(vocab))
+
+# 4) Simple tokenizer class v2
+
+class SimpleTokenizerV2:
+    def __init__(self, vocab):
+        self.str_to_int = vocab
+        self.int_to_str = {i:s for s,i in vocab.items()}
+
+    def encode(self, text):
+        preprocessed = re.split(r'([,.?_!"()\']|--|\s)', text)
+        preprocessed = [
+            item.strip() for item in preprocessed if item.strip()
+        ]
+        preprocessed = [item if item in self.str_to_int
+                        else "<|unk|>" for item in preprocessed]
+        
+        ids = [self.str_to_int[s] for s in preprocessed]
+        return ids
+
+    def decode(self, ids):
+        text = " ".join(self.int_to_str[i] for i in ids)
+        text = re.sub(r'\s+([,.?!"()\'])', r'\1', text)
+        return text
+    
+# Test
+tokenizer_v2 = SimpleTokenizerV2(vocab)
+ids = tokenizer_v2.encode(raw_text)
+print("First 20 tokens:", tokens[:20])
+print("First 20 IDs   :", ids[:20])
