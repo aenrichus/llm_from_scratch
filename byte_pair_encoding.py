20d9213f50e8b89f48edc4fcdb6269a334cb2898
Henry Wolf <henry@henrywolf.ai>
2025-04-26 15:35:14 -1000

    adding bpe tokenization

---
+++ byte_pair_encoding.py
@@ -0,0 +1,14 @@
+from importlib.metadata import version
+import tiktoken
+print(f"tiktoken version: {version('tiktoken')}")
+
+tokenizer = tiktoken.get_encoding("gpt2")
+
+with open('decay.txt', 'r', encoding='utf-8') as file:
+    raw_text = file.read()
+
+integers = tokenizer.encode(raw_text, allowed_special="all")
+print("First 20 tokens:", integers[:20])
+
+strings = tokenizer.decode(integers)
+print("First 20 tokens:", strings[:200])
