de9a54bdeea2443f8b867fe891d69a32e2ce5240
Henry Wolf <henry@henrywolf.ai>
2025-05-02 12:49:16 -1000

    implemented multi-head attention and completed testing

--- trainable_attention.py
+++ trainable_attention.py
@@ -184,3 +184,52 @@ print(context_vecs)
 print(context_vecs.shape)
 
 # Efficient multi-head attention
+class MultiHeadAttention(nn.Module):
+    def __init__(self, d_in, d_out, context_length, dropout, num_heads, qkv_bias=False):
+        super().__init__()
+        assert (d_out % num_heads == 0), "d_out must be divisible by num_heads"
+
+        self.d_out = d_out
+        self.num_heads = num_heads
+        self.head_dim = d_out // num_heads
+        self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_key = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.out_proj = nn.Linear(d_out, d_out)
+        self.dropout = nn.Dropout(dropout)
+        self.register_buffer("mask", torch.triu(torch.ones(context_length, context_length), diagonal=1))
+    
+    def forward(self, x):
+        b, num_tokens, d_in = x.shape
+        key = self.W_key(x)
+        query = self.W_query(x)
+        value = self.W_value(x)
+
+        key = key.view(b, num_tokens, self.num_heads, self.head_dim)
+        value = value.view(b, num_tokens, self.num_heads, self.head_dim)
+        query = query.view(b, num_tokens, self.num_heads, self.head_dim)
+
+        key = key.transpose(1, 2)
+        value = value.transpose(1, 2)
+        query = query.transpose(1, 2)
+
+        attn_scores = torch.matmul(query, key.transpose(2, 3))
+        mask_bool = self.mask.bool()[:num_tokens, :num_tokens]
+
+        attn_scores.masked_fill_(mask_bool, -torch.inf)
+        attn_weights = torch.softmax(attn_scores / keys.shape[-1]**0.5, dim=-1)
+        attn_weights = self.dropout(attn_weights)
+
+        context_vector = torch.matmul(attn_weights, value).transpose(1, 2)
+        context_vector = context_vector.contiguous().view(b, num_tokens, self.d_out)
+        output = self.out_proj(context_vector)
+        return output
+
+torch.manual_seed(123)
+batch_size, context_length, d_in = batch.shape
+d_out = 2
+mha = MultiHeadAttention(d_in, d_out, context_length, 0.0, 2)
+context_vecs = mha(batch)
+print(context_vecs)
+print(context_vecs.shape)
+
