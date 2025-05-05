89346f5d9894d0bafbd193f5dd3b43ffd20596bf
Henry Wolf <henry@henrywolf.ai>
2025-05-04 15:06:36 -1000

    implemented layer normalization class and completed testing

--- gpt_model.py
+++ gpt_model.py
@@ -70,3 +70,48 @@ model = DummyGPTModel(GPT_CONFIG_124M)
 logits = model(batch)
 print(logits.shape)
 print(logits)
+
+# Layer normalization examples (without and with normalization)
+torch.manual_seed(123)
+batch_example = torch.randn(2, 5)
+layer = nn.Sequential(nn.Linear(5, 6), nn.ReLU())
+out = layer(batch_example)
+print(out)
+mean = out.mean(dim=-1, keepdim=True)
+var = out.var(dim=-1, keepdim=True)
+print(mean)
+print(var)
+
+out_norm = (out - mean) / torch.sqrt(var)
+mean = out_norm.mean(dim=-1, keepdim=True)
+var = out_norm.var(dim=-1, keepdim=True)
+print(out_norm)
+print(mean)
+print(var)
+
+torch.set_printoptions(precision=2, sci_mode=False)
+print(mean)
+print(var)
+
+# Implementation of LayerNorm
+class LayerNorm(nn.Module):
+    def __init__(self, emb_dim):
+        super().__init__()
+        self.eps = 1e-5
+        self.scale = nn.Parameter(torch.ones(emb_dim))
+        self.shift = nn.Parameter(torch.zeros(emb_dim))
+
+    def forward(self, x):
+        mean = x.mean(dim=-1, keepdim=True)
+        var = x.var(dim=-1, keepdim=True)
+        norm_x = (x - mean) / torch.sqrt(var + self.eps)
+        return self.scale * norm_x + self.shift
+
+# Test LayerNorm
+ln = LayerNorm(emb_dim=5)
+out_ln = ln(batch_example)
+mean = out_ln.mean(dim=-1, keepdim=True)
+var = out_ln.var(dim=-1, keepdim=True)
+print(out_ln)
+print(mean)
+print(var)
