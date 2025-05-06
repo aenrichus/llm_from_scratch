3339d9511f3fa69fa0bbda84e5d76e0a8cf790aa
Henry Wolf <henry@henrywolf.ai>
2025-05-06 09:44:01 -1000

    added config for pretraining gpt2 124M

---
+++ pretraining.py
@@ -0,0 +1,17 @@
+import torch
+import torch.nn as nn
+from gpt_model import GPTModel
+
+GPT_CONFIG_124M = {
+    "vocab_size": 50257,
+    "context_length": 256,
+    "emb_dim": 768,
+    "n_heads": 12,
+    "n_layers": 12,
+    "drop_rate": 0.1,
+    "qkv_bias": False
+}
+
+torch.manual_seed(123)
+model = GPTModel(GPT_CONFIG_124M)
+model.eval()
