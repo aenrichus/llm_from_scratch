d4d8b593967f87985bde9332315da43abaa51cbc
Henry Wolf <henry@henrywolf.ai>
2025-05-07 16:31:42 -1000

    added functions for training and evaluating the model, trained for 10 epochs and visualized the loss

--- pretraining.py
+++ pretraining.py
@@ -85,3 +85,177 @@ print("Targets flat shape:", targets_flat.shape)
 loss = nn.functional.cross_entropy(logits_flat, targets_flat)
 print("Loss:", loss)
 print(loss.item())
+
+file_path = 'decay.txt'
+with open(file_path, 'r', encoding='utf-8') as file:
+    raw_text = file.read()
+
+total_characters = len(raw_text)
+total_tokens = len(tokenizer.encode(raw_text))
+print(f"Total characters: {total_characters}")
+print(f"Total tokens: {total_tokens}")
+
+train_ratio = 0.9
+split_idx = int(total_characters * train_ratio)
+train_data = raw_text[:split_idx]
+val_data = raw_text[split_idx:]
+print(f"Training data size: {len(train_data)}")
+print(f"Validation data size: {len(val_data)}")
+
+from dataloader import create_dataloader_v1
+torch.manual_seed(123)
+
+train_loader = create_dataloader_v1(
+    train_data, 
+    batch_size=2, 
+    max_length=GPT_CONFIG_124M["context_length"], 
+    stride=GPT_CONFIG_124M["context_length"], 
+    shuffle=True, 
+    drop_last=True, 
+    num_workers=0
+)
+val_loader = create_dataloader_v1(
+    val_data, 
+    batch_size=2, 
+    max_length=GPT_CONFIG_124M["context_length"], 
+    stride=GPT_CONFIG_124M["context_length"], 
+    shuffle=False, 
+    drop_last=False, 
+    num_workers=0
+)
+
+print("Train loader:")
+for x, y in train_loader:
+    print(x.shape, y.shape)
+
+print("Validation loader:")
+for x, y in val_loader:
+    print(x.shape, y.shape)
+
+def calc_loss_batch(input_batch, target_batch, model, device):
+    input_batch = input_batch.to(device)
+    target_batch = target_batch.to(device)
+    logits = model(input_batch)
+    logits_flat = logits.flatten(0, 1)
+    targets_flat = target_batch.flatten()
+    loss = nn.functional.cross_entropy(logits_flat, targets_flat)
+    return loss
+
+def calc_loss_loader(data_loader, model, device, num_batches=None):
+    total_loss = 0.0
+
+    if len(data_loader) == 0:
+        return float("nan")
+    elif num_batches is None:
+        num_batches = len(data_loader)
+    else:
+        num_batches = min(num_batches, len(data_loader))
+    
+    for i, (input_batch, target_batch) in enumerate(data_loader):
+        if i < num_batches:
+            loss = calc_loss_batch(input_batch, target_batch, model, device)
+            total_loss += loss.item()
+        else:
+            break
+    
+    avg_loss = total_loss / num_batches
+    return avg_loss
+
+device = torch.device("mps" if torch.mps.is_available() else "cpu")
+print("Device:", device)
+
+model.to(device)
+with torch.no_grad():
+    train_loss = calc_loss_loader(train_loader, model, device)
+    val_loss = calc_loss_loader(val_loader, model, device)
+print(f"Train loss: {train_loss:.4f}")
+print(f"Validation loss: {val_loss:.4f}")
+
+# Training an LLM
+def train_model_simple(model, train_loader, val_loader, optimizer, device, num_epochs, eval_freq, eval_iter, start_context, tokenizer):
+    train_losses, val_losses, track_tokens_seen = [], [], []
+    tokens_seen, global_step = 0, -1
+
+    for epoch in range(num_epochs):
+        model.train()
+        for input_batch, target_batch in train_loader:
+            optimizer.zero_grad()
+            loss = calc_loss_batch(input_batch, target_batch, model, device)
+            loss.backward()
+            optimizer.step()
+            tokens_seen += input_batch.numel()
+            global_step += 1
+
+            if global_step % eval_freq == 0:
+                train_loss, val_loss = evaluate_model(model, train_loader, val_loader, device, eval_iter)
+                train_losses.append(train_loss)
+                val_losses.append(val_loss)
+                track_tokens_seen.append(tokens_seen)
+                print(f"Epoch* {epoch+1}, (Step {global_step:06d}) :" 
+                      f"Train Loss: {train_loss:.4f}, "
+                      f"Val Loss: {val_loss:.4f}")
+        
+        generate_and_print_sample(model, tokenizer, device, start_context)
+    return train_losses, val_losses, track_tokens_seen
+
+def evaluate_model(model, train_loader, val_loader, device, eval_iter):
+    model.eval()
+    with torch.no_grad():
+        train_loss = calc_loss_loader(train_loader, model, device, num_batches=eval_iter)
+        val_loss = calc_loss_loader(val_loader, model, device, num_batches=eval_iter)
+    model.train()
+    return train_loss, val_loss
+
+def generate_and_print_sample(model, tokenizer, device, start_context):
+    model.eval()
+    context_size = model.pos_emb.weight.shape[0]
+    encoded = text_to_token_ids(start_context, tokenizer).to(device)
+    with torch.no_grad():
+        token_ids = generate_text_simple(model=model,
+                                         idx=encoded,
+                                         max_new_tokens=50,
+                                         context_size=context_size)
+    decoded_text = token_ids_to_text(token_ids, tokenizer)
+    print(decoded_text.replace("\n", " "))
+    model.train()
+
+torch.manual_seed(123)
+model = GPTModel(GPT_CONFIG_124M)
+model.to(device)
+optimizer = torch.optim.AdamW(model.parameters(), lr=1e-4, weight_decay=1e-1)
+num_epochs = 10
+train_losses, val_losses, tokens_seen = train_model_simple(
+    model, train_loader, val_loader, optimizer, device, num_epochs=num_epochs,
+    eval_freq=5, eval_iter=5, start_context="Every effort moves you", tokenizer=tokenizer)
+
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
+def plot_losses(epochs_seen, tokens_seen, train_losses, val_losses):
+    fig, ax1 = plt.subplots(figsize=(5, 3))
+    ax1.plot(epochs_seen, train_losses, label='Train Loss', color='blue')
+    ax1.plot(epochs_seen, val_losses, label='Validation Loss', color='orange')
+    ax1.set_xlabel('Epochs')
+    ax1.set_ylabel('Loss')
+    ax1.legend(loc='upper right')
+    ax1.xaxis.set_major_locator(MaxNLocator(integer=True))
+    ax2 = ax1.twiny()
+    ax2.plot(tokens_seen, train_losses, alpha=0)
+    ax2.set_xlabel('Tokens Seen')
+    fig.tight_layout()
+    plt.show()
+
+epochs_tensor = torch.linspace(0, num_epochs, len(train_losses))
+plot_losses(epochs_tensor, tokens_seen, train_losses, val_losses)
+
+model.to("cpu")
+model.eval()
+tokenizer = tiktoken.get_encoding("gpt2")
+token_ids = generate_text_simple(
+    model=model, 
+    idx=text_to_token_ids("Every effort moves you", tokenizer),
+    max_new_tokens=25,
+    context_size=GPT_CONFIG_124M['context_length']
+)
+print(token_ids_to_text(token_ids, tokenizer))
+
+## Temperature scaling
